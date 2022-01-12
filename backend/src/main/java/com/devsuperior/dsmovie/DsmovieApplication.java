ed48feb3a616c7c31222428ebdcbaf178639fb8b
jesusapmoro <jesusapmoro@gmail.com>
2022-01-12 08:48:51 -0300

    Security config

--- backend/src/main/java/com/devsuperior/dsmovie/DsmovieApplication.java
+++ backend/src/main/java/com/devsuperior/dsmovie/DsmovieApplication.java
@@ -1,4 +1,4 @@
-package devsuperior.dsmovie;
+package com.devsuperior.dsmovie;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
